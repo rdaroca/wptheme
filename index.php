b81afd5ec8ea548f1be99d0c95c9bf94974b3ed2
rdaroca <richarddaroca@gmail.com>
2017-04-20 22:36:30 +0800

    product page

--- index.php
+++ index.php
@@ -1,62 +1,28 @@
 
 		<?php get_header(); ?>
 
-<!-- 		<div class="main_content clearfix">
-
-			<div class="featured clearfix">
-				<div class="featured_product">
-					<a href="#"><img src="../Static Page copy/images/fproduct512.png"></a>
-				</div>
-
-				<div class="featured_content">
-					<h1>Horizon</h1>
-					<h2>ZeroDawn</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <a href="#" class="readmore">ReadMore</a></p>
-				</div>
-
-			</div>
-
-			<div class="other_products clearfix">
-				<ul class="listings">
-					<li class="post_wrapper">
-						<div class="thumbnail">
-							<img src="../Static Page copy/images/uncharted4.png">
-						</div>
-						<h2>Uncharted 4</h2>
-						<h3>Lorem ipsum dolor sit amet, consectetur adipiscsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <a href="#" class="readmore">ReadMore</a></h3>
-					</li>
-
-					<li class="post_wrapper">
-						<div class="thumbnail">
-							<img src="../Static Page copy/images/darksouls3.png">
-						</div>
-						<h2>DarkSouls 3</h2>
-						<h3> In omnis habemus similique vix. Mel te quodsi adipiscing, nostro commodo mediocrem per cu, quo natum tempor libris ex. Fugit decore labores cu cum, inani denique quaerendum sit ut, in vel dicant audire dignissim. Vix ex convenire complectitur, sit phaedrum reprehendunt id, <a href="#" class="readmore">ReadMore</a></h3>
-					</li> 
-
-					<li class="post_wrapper">
-						<div class="thumbnail">
-							<img src="../Static Page copy/images/metalgear.png">
-						</div>
-						<h2>Metal Gear</h2>
-						<h3>Lorem ipsum dolor sit amet, consectetur adipiscsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <a href="#" class="readmore">ReadMore</a></h3>
-					</li>
-
+			<div class="post clearfix">
+				<ul>
+				<?php if ( have_posts() ) : ?>  <!-- https://codex.wordpress.org/The_Loop -->
+				    <?php while ( have_posts() ) : the_post(); ?>
+				    	<li class="post_list">
+					    	<div class="post_thumbnail">
+					    	<?php
+							
+							if ( has_post_thumbnail()) {		//checks if a featured image exist if yes display full size of the image
+								the_post_thumbnail('full');		// output is equal to <img src=“” />
+							}
+							
+							?>
+							</div>
+							<div class="post_content">
+					        <h2><?php the_title(); ?></h2>  <!-- https://codex.wordpress.org/Template_Tags -->
+					        <p><?php the_excerpt(); ?></p>
+					        </div>
+					    </li>
+				    <?php endwhile; ?>
+				<?php endif; ?>
 				</ul>
-
 			</div>
-
-		</div> -->
-
-
-		<?php if ( have_posts() ) : ?>  <!-- https://codex.wordpress.org/The_Loop -->
-		    <?php while ( have_posts() ) : the_post(); ?>
-
-		        <h2><?php the_title(); ?></h2>  <!-- https://codex.wordpress.org/Template_Tags -->
-		        <p><?php the_excerpt(); ?></p>
-
-		    <?php endwhile; ?>
-		<?php endif; ?>
-
 
 		<?php get_footer(); ?>
